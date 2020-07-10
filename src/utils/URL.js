7be6bc6010bb38380c0f5e371c351ba56db9a26f
Rayjens Code <rayjenscode@gmail.com>
2020-07-10 18:37:52 +0800

    update url

--- src/utils/URL.js
+++ src/utils/URL.js
@@ -1,3 +1,4 @@
 // url
 // export default 'http://localhost:1337';
-export default 'https://reacr-tech-store-api.herokuapp.com';
+// export default 'https://reacr-tech-store-api.herokuapp.com';
+export default 'https://pacific-brushlands-91379.herokuapp.com';
